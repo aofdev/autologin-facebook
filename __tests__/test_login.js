206f1e7cff0c0d9080d2c6aa1b987851af74202a
Visarut Phusua <aof.dev@gmail.com>
2018-06-15 16:28:29 +0700

    comment testing

--- __tests__/test_login.js
+++ __tests__/test_login.js
@@ -1,23 +1,23 @@
-const timeout = 11000
-const facebook = require('../login_facebook')
+// const timeout = 11000
+// const facebook = require('../login_facebook')
 
-describe(
-  'Test Login',
-  () => {
-    let page
-    beforeAll(async () => {
-      page = await global.__BROWSER__.newPage()
-      await facebook.LOGIN(page, '', '') // username, paswword
-    }, timeout)
+// describe(
+//   'Test Login',
+//   () => {
+//     let page
+//     beforeAll(async () => {
+//       page = await global.__BROWSER__.newPage()
+//       await facebook.LOGIN(page, '', '') // username, paswword
+//     }, timeout)
 
-    afterAll(async () => {
-      await page.close()
-    })
+//     afterAll(async () => {
+//       await page.close()
+//     })
 
-    it('should load without error', async () => {
-      const name = await page.evaluate(() => document.querySelector('span._1vp5').textContent)
-      expect(name).toContain('') // name facebook
-    })
-  },
-  timeout
-)
+//     it('should load without error', async () => {
+//       const name = await page.evaluate(() => document.querySelector('span._1vp5').textContent)
+//       expect(name).toContain('') // name facebook
+//     })
+//   },
+//   timeout
+// )
